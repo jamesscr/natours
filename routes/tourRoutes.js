c8cc8a96a4c3c2d62f5ef260c74b839fdca2b7cf
James Allan Jean-Jacques <allanjeanjacques@hotmail.com>
2021-06-13 12:29:12 -0400

    update 404 CkeckID

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+router.param('id', tourController.ckeckID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
