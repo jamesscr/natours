0da273d94ff6a6a966e5a6d613ff744e552029b7
James Allan Jean-Jacques <allanjeanjacques@hotmail.com>
2021-06-13 20:55:04 -0400

    ad DB

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('db connection success!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
